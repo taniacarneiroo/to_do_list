ac2479b10f74e3118052506ef13b0849072800e9
Tania Carneiro <tania.carneiro@pbh.gov.br>
2023-02-07 11:08:30 -0300

    Inserção de exclusão de tarefas.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,8 +7,17 @@ exibe/grava em algum lugar
 /*Criar função para armazenar o que for inserido no index.html */
 let tarefas = [];
 
-function adicionarTarefas(){
+function adicionarTarefas(event){
     let tarefa = document.querySelector('#tarefa');
+
+  
+
+if (event.key == 'Enter' || event.type == 'click')
+
+
+
+{
+    
 
     tarefas.push(
         {
@@ -16,10 +25,12 @@ function adicionarTarefas(){
             status: 'à executar'
         }
         )
-        tarefa.value='';
+        tarefa.value=''; 
 
         exibeTarefas();
-    }
+
+       
+    }}
        
 function exibeTarefas(){
     let cardTarefas = document.querySelector('section');
@@ -34,9 +45,29 @@ function exibeTarefas(){
             <button onclick='editarTarefas()'>Editar</button>
             <button onclick='excluiTarefas()'>Excluir</button>
         </span>`;
+    }}
+
+function excluiTarefas(event){
+    let elementoPai = event.target.parentNode;
+
+    let tituloTarefa = elementoPai.children[0].innerText;
+    let statusTarefa = elementoPai.children[1].innerText;
+
+    if(statusTarefa ==='Feito'){
+        alert('Não é possível excluir a tarefa com o status' + statusTarefa);
+        return;
     }
 
-    
+for (let [index,tarefa] of tarefas.entries()){
+    if(tituloTarefa === tarefa.titulo) {
+        tarefas.splice(index,1);
+    }
+};
+
+elementoPai.remove();
+
+
+
 }
 
 
